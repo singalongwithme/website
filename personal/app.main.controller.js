e379130a6fdefe6f179ac3c1640037c658bb1c53
singalongwithme <csinghavong@gmail.com>
2015-01-19 17:58:20 -0800

    add dependencies

--- personal/app.main.controller.js
+++ personal/app.main.controller.js
@@ -6,16 +6,16 @@
   .controller('MainController', MainController);
 
   function MainController($scope){
-  	$scope.todos = [
+    $scope.todos = [
       {
         face : 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQl7UQ2N91uExrer8I9Dim_pAqomv9goJbbNCVqn-WlPi8xURau',
         what: 'Basketball',
-        who: 'I\'ve been playing basketball since I was a kid and still profess that it\'s the most fun medium of excercise on the planet.'
+        who: 'I\'ve been playing basketball since I was a kid and still contest that it\'s the greatest medium of excercise on the planet.'
       },
       {
         face : 'http://www.clipartbest.com/cliparts/niE/Rjk/niERjkxiA.gif',
         what: 'Music',
-        who: 'Music is one of those things I was glad my parents forced me to do. I definitely love playing guitar like John Mayer, or at least trying to.'
+        who: 'Music is one of those things I was glad my parents forced me to do. I play the guitar like John Mayer, or at least I try.'
       },
       {
         face : 'http://www.chicagonow.com/tween-us/files/2012/05/video-game-controller.jpg',
