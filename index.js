ae22b29732d2ba20883f096978b6140607716a28
singalongwithme <csinghavong@gmail.com>
2015-01-26 18:36:58 -0800

    add node and express

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/personal'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
 });
 
 app.listen(app.get('port'), function() {
