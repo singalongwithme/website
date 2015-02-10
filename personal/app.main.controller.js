bcb1345b6e3acc7cfdbe88be5e15ae7e2548657c
singalongwithme <csinghavong@gmail.com>
2015-02-09 18:06:50 -0800

    fix styling

--- personal/app.main.controller.js
+++ personal/app.main.controller.js
@@ -8,7 +8,7 @@
   function MainController(){
   	function parallax() {
   		var about = document.getElementById('about-me');
-  	  about.style.top = -( window.pageYOffset / 2 ) + 338 + 'px'
+  	  about.style.top = -( window.pageYOffset / 2 ) + 425 + 'px'
   	}
 
   	window.addEventListener("scroll", parallax, false);
